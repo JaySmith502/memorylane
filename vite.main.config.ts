bef866dcc443b084366bfa849086327a86fd7792
Filip Kubiš <fkubis@icloud.com>
2026-02-04 17:28:39 +0100

    Add debug configuration and sourcemaps to vite config to enable debugging

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
+    sourcemap: true,
     rollupOptions: {
       external: ['uiohook-napi', '@lancedb/lancedb', 'onnxruntime-node', 'onnxruntime-common', 'sharp', 'active-win'],
     },
