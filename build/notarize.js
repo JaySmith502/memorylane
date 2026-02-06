471da5cda18800fd586e3a1894616fe92498e50b
Petr Ungar <petr.ungar@gmail.com>
2026-02-06 15:12:27 +0100

    do not notarize the app without apple id

--- build/notarize.js
+++ build/notarize.js
@@ -6,6 +6,11 @@ exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
   if (electronPlatformName !== 'darwin') return
 
+  if (!process.env.APPLE_ID || !process.env.APPLE_APP_PASSWORD) {
+    console.log('Skipping notarization: APPLE_ID or APPLE_APP_PASSWORD not set.')
+    return
+  }
+
   const appName = context.packager.appInfo.productFilename
 
   console.log(`Notarizing ${appName}...`)
