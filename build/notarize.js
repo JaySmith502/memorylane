c8faad1871cf22822ee0e03ff9fa8495ee0681d1
Filip Kubiš <fkubis@icloud.com>
2026-02-06 11:04:03 +0100

    suppress js warnings

--- build/notarize.js
+++ build/notarize.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
 const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
