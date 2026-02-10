77e8e8f46788c9a1633a0a88b21e0ad6e7ec8454
Filip Kubiš <fkubis@icloud.com>
2026-02-10 19:21:32 +0100

    remove stripe for now

--- src/renderer/pages/main-window/components/ApiKeySetupSection.tsx
+++ src/renderer/pages/main-window/components/ApiKeySetupSection.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { SubscribeColumn } from './SubscribeColumn'
 import { BringYourOwnKeyColumn } from './BringYourOwnKeyColumn'
 import type { MainWindowAPI } from '@types'
 
@@ -10,8 +9,7 @@ interface ApiKeySetupSectionProps {
 
 export function ApiKeySetupSection({ api, onKeySet }: ApiKeySetupSectionProps): React.JSX.Element {
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <SubscribeColumn api={api} onKeySet={onKeySet} />
+    <div className="grid grid-cols-1 gap-4">
       <BringYourOwnKeyColumn api={api} onKeySet={onKeySet} />
     </div>
   )
